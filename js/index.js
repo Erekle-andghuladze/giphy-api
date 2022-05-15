5465e2c596a65d3cc83c15390bf640090646b1c2
Erekle <erekleandghuladze@gmail.com>
2022-05-16 01:29:11 +0400

    recent search buttons

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-const apiKey = 'bpHGwR19nADNvbNZ3ZdtQrRRTFLcuTu0';
-let recentSearches = [];
+const apiKey = 'Rx02GemP9C4kA4m2LAiqMIOsAMdigsKp';
+
+const recentsLimit = 6;
+let recentSearches = ["lotr", "sponge bob", "for honor", "total war", "assassin's creed"];
 
 const getTrendingResults = () => {
     clearResults();
@@ -38,22 +40,65 @@ const clearResults = () => {
     gifs.innerHTML = ""
 }
 
-const search = () => {
+const searchListener = () => {
     let searchSubmit = document.getElementById("submit");
     searchSubmit.addEventListener("click", () => {
         let searchBarValue = document.getElementById("user-search").value;
+        let parsedString = searchBarValue.replace(/\s/g, '');
+        if(parsedString.length > 0){
         getSearchResults(searchBarValue);
+        recentSearches.push(searchBarValue);
+        updateRecentButtons(searchBarValue);
+        createRecentButtons();
+        console.log(recentSearches)
+        }
     });
 }
 
-const trending = () => {
+const trendingListener = () => {
     let trendingButton = document.getElementById("trending");
     trendingButton.addEventListener("click", () => {
         getTrendingResults();
     });
 }
 
+const createRecentButtons = () => {
+    document.getElementById("buttons").innerHTML = ""
+    for(let i=0; i < recentSearches.length; i++) {
+        createRecentButton(recentSearches[i], i);
+    }
+    recentButtonSearchListener();
+}
 
-search();
-trending();
+const createRecentButton = (recentSearch, idIndex) => {
+    let button = document.createElement("button");
+    button.id = `recents${idIndex}`;
+    button.innerHTML = recentSearch;
+    document.getElementById("buttons").appendChild(button);
+}
 
+const updateRecentButtons = lastSearch => {
+    while(recentSearches.length > recentsLimit) {
+        recentSearches.shift();
+    }
+    createRecentButton(lastSearch, recentSearches[recentSearches.length]);
+}
+
+const recentButtonSearchListener = () => {
+    let length = recentsLimit > recentSearches.length ? recentSearches.length : recentsLimit;
+    console.log(length);
+
+    for(let i=0; i<length; i++) {
+        let button = document.getElementById(`recents${i}`);
+        console.log(`recents${i}`)
+        console.log(button.innerHTML);
+        button.addEventListener("click", () => {
+            getSearchResults(button.innerHTML);
+        });
+    }
+}
+
+
+createRecentButtons();
+searchListener();
+trendingListener();
